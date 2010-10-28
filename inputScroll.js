220df86a8c37d0874f18938058d68afcc9530576
Sean Gaffney <seangaffney@gmail.com>
2010-10-28 16:23:00 -0400

    First commit

---
+++ inputScroll.js
@@ -0,0 +1,45 @@
+$.fn.roll = function(direction, amount) {
+  
+  if (typeof rolling == 'undefined') {
+    rolling = false;
+  }
+  
+  if (!rolling) {
+    rolling = true;
+    amount = amount || 10;
+    self = $(this);
+    $newAmount = self.clone();
+    
+    self.attr('name', self.attr('name')+'_old');
+    
+    dollas = parseInt(self.val().replace('$', ''));
+  
+    if (direction == 'down') {
+      if (dollas <= 0) {
+        dollas = 0;
+      } else {
+       dollas -= amount;
+       go = false;
+      }
+      modifier = '';
+      move = '-';
+    } else {
+      dollas += amount;
+      modifier = '-';
+      move = '+';
+    }
+  
+    $newAmount.val('$'+dollas).css('top', modifier+'30px');
+ 
+    self.parent().append($newAmount);
+  
+    $('.'+self.attr('class')).stop(true, true).animate({top: move + '=30'}, 200, function(){
+      self.remove();
+      rolling = false;
+    });
+  
+    return true;
+  } else {
+    return false;
+  }
+}
